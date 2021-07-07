f7fca8571c3b147ccd0257238d947b2b711f0760
Olly Hayes <olly.hayes@gmail.com>
2021-07-07 15:24:19 +0100

    Add cadence scanner

--- src/ant.js
+++ src/ant.js
@@ -1,25 +1,37 @@
+const { GarminStick2, CadenceScanner } = require('ant-plus');
 const Ant = require('ant-plus');
 
 function connectAnt(emitHeartRate) {
-	const stick = new Ant.GarminStick3();
+	const stick = new GarminStick2(3);
 
-	const sensor = new Ant.HeartRateScanner(stick);
+	const cadenceScanner = new CadenceScanner(stick);
 
-	sensor.on('attached', () => {
-		console.log(`Heart rate sensor attached, channel: ${sensor.channel}, deviceID: ${sensor.deviceID}`);
+	const heartScanner = new Ant.HeartRateScanner(stick);
+
+	heartScanner.on('attached', () => {
+		console.log(`Heart rate scanner attached, channel: ${heartScanner.channel}, deviceID: ${heartScanner.deviceID}`);
 	});
 
-	sensor.on('hbData', function (data) {
-		console.log(`Data received, deviceID: ${sensor.deviceID}, heart rate: ${data.computedHeartRate}`);
+	cadenceScanner.on('attached', () => {
+		console.log(`Cadence scanner attached, channel: ${heartScanner.channel}, deviceID: ${heartScanner.deviceID}`);
+	});
+
+	heartScanner.on('hbData', function (data) {
+		console.log(`Data received, deviceID: ${heartScanner.deviceID}, heart rate: ${data.computedHeartRate}`);
 
 		// emitHeartRate(sensor.deviceID, data);
 		emitHeartRate(data);
+	});
+
+	cadenceScanner.on('speedData', function (data) {
+		console.log(`Data received, deviceID: ${cadenceScanner.deviceID}, heart rate: ${JSON.stringify(data)}`);
 	});
 	
 	stick.on('startup', function () {
 		//  sensor.attach(0, 0);
 		console.log('Startup event received, scanning...');
-		sensor.scan();
+		heartScanner.scan();
+		cadenceScanner.scan();
 	});
 
 	console.log('Opening stick...');
